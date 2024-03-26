73667fb9e051240b641a015ec2eced42d7d5eae3
opa334 <opa334@users.noreply.github.com>
2024-03-26 21:19:57 +0100

    Add qol script for testing changes

---
+++ copy_to_theos.sh
@@ -0,0 +1,4 @@
+cp libroot_dyn_iphoneos-arm.a $THEOS/vendor/lib/libroot.a
+cp libroot_dyn_iphoneos-arm.oldabi.a $THEOS/vendor/lib/libroot_oldabi.a
+cp libroot_dyn_iphoneos-arm64.a $THEOS/vendor/lib/iphone/rootless/libroot.a
+cp libroot_dyn_iphoneos-arm64.oldabi.a $THEOS/vendor/lib/iphone/rootless/libroot_oldabi.a
